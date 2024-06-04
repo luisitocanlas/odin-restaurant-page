3544805e442c33c58141ea87351d6626e2e0fbdd
Luisito Canlas <luisitocanlas@gmail.com>
2024-06-04 15:43:32 -0700

    Imported styling and console log for dev or prod

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import './styles/style.css';
 import initialPageLoad from './initialPageLoad.js';
 import loadMenu from './menu.js';
 import loadAbout from './about.js';
+
+if (process.env.NODE_ENV !== 'production') {
+	console.log('Looks like we are in development mode!');
+} else {
+	console.log('Looks like we are in production mode!');
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 	initialPageLoad();
