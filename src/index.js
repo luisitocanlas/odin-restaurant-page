44f8e37050d22d191a3b7bc960c93facb740545e
Luisito Canlas <luisitocanlas@gmail.com>
2024-06-04 14:40:36 -0700

    Added nav button functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import initialPageLoad from './initialPageLoad.js';
+import loadMenu from './menu.js';
+import loadAbout from './about.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	initialPageLoad();
+
+	const homeBtn = document.querySelector('#homeBtn');
+	const menuBtn = document.querySelector('#menuBtn');
+	const aboutBtn = document.querySelector('#aboutBtn');
+
+	homeBtn.addEventListener('click', initialPageLoad);
+	menuBtn.addEventListener('click', loadMenu);
+	aboutBtn.addEventListener('click', loadAbout);
 });
